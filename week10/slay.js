988b8088aad597cc09da26d40b0c5c5f79bc40e6
Lizzie England <lizzieengland@Lizzies-MacBook-Air.local>
2022-11-12 19:40:14 -0500

    p

--- week10/slay.js
+++ week10/slay.js
@@ -1,13 +1,24 @@
+console.log("the message!")
 
+
+let theUser = "rebecca"
+let theNumber = 800000
+console.log(theUser + " " + theNumber)
+// legacey variable -- var
+// ES6: let , const
 
 const theButton = document.querySelector('button')
+console.log(theButton)
+let theMain = document.querySelector('main')
+let theTxt = document.getElementById('theText')
+
 theButton.addEventListener('click', changeColor)
 
+
 function changeColor(){
-  console.log("horray!")
-  theTxt.textContent = "you clicked the button!"
-  theMain.style.backgroundColor = "yellow"
-  
+    console.log("horray!")
+    theTxt.textContent = "you clicked the button!"
+    theMain.style.backgroundColor = "yellow"
+    
 
 }
-
