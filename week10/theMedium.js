b87c11f95b42042c251ec4ec9510bd0a859584d3
Lizzie England <lizzieengland@Lizzies-MacBook-Air.local>
2022-11-13 23:00:13 -0500

    bruh

--- week10/theMedium.js
+++ week10/theMedium.js
@@ -1,26 +1,35 @@
-
+let popup = document.getElementById("popup")
 const theButton = document.querySelector('button')
+const twoButton = document.querySelector('button')
 console.log(theButton)
 let theMain = document.querySelector('main')
 let theTxt = document.getElementById('theText')
+
+
+function openPopup(){
+      popup.classList.add("open-popup");
+      overlay.classList.add('active')
+  }
+function closePopup(){
+      popup.classList.remove("open-popup");
+      overlay.classList.remove('active')
+  }
 
 const unicornSpinning = [
     { transform: 'rotate(0) scale(1)' },
     { transform: 'rotate(360deg) scale(0)' }
   ];
   
-  const unicornTiming = {
+const unicornTiming = {
     duration: 4000,
     iterations: 1,
   }
 
 theButton.addEventListener('click', changeText)
-
+tButton.addEventListener('click', changeText)
 
 function changeText(){
     console.log("slay!")
-    theTxt.textContent = "uh oh...i think you might have killed her..."
+    theTxt.textContent = "keep clicking the unicorns (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧"
     theButton.animate(unicornSpinning, unicornTiming);
-
 }
-
